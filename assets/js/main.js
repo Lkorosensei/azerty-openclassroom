9509a252e99d618047a43c9ad71904a5b5268073
isamir <samir.ibrahim38@outlook.fr>
2023-11-27 10:14:33 +0100

    avancee

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,19 +6,19 @@
 
 lancerJeu()
 
-// Methode getElementById
-let inputEcriture = document.getElementById("inputEcriture");
-let btnValiderMot = document.getElementById("btnValiderMot");
-console.log("c'est l'endroit où je vais saisir mon texte : ", inputSaisieTexte);
-console.log("c'est le bouton pour valider : ",btnValider);
+// // Methode getElementById
+// let inputEcriture = document.getElementById("inputEcriture");
+// let btnValiderMot = document.getElementById("btnValiderMot");
+// console.log("c'est l'endroit où je vais saisir mon texte : ", inputSaisieTexte);
+// console.log("c'est le bouton pour valider : ",btnValider);
 
-// Methode querySelector
-let motAfficher = document.querySelector(".zoneProposition");
-let score = document.querySelector(".zoneScore span")
-console.log("c'est l'endroit ou les mots vont être affiché : ", motAfficher);
-console.log("c'est le score : ", score);
+// // Methode querySelector
+// let motAfficher = document.querySelector(".zoneProposition");
+// let score = document.querySelector(".zoneScore span")
+// console.log("c'est l'endroit ou les mots vont être affiché : ", motAfficher);
+// console.log("c'est le score : ", score);
 
-// Methode querySelectorAll
-let btnRadioChoice = document.querySelectorAll("input[type='radio']")
-console.log("c'est les boutons radio : ", btnRadioChoice);
+// // Methode querySelectorAll
+// let allBtnRadio = document.querySelectorAll("input[type='radio']")
+// console.log("c'est les boutons radio : ", allBtnRadio);
 
